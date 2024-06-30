7151c547278d8ee220866ec66c6411d01cb519d1
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-06-30 10:22:09 +0200

    add: api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Technology;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $projects = Project::with('technologies')->get();
+
+        $data = [
+            'success' => true,
+            'results' => $projects
+        ];
+
+        return response()->json($data);
+    }
 }
+
